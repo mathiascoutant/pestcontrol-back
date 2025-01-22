df9d5ba992dfe1266e46d24b2122fa1f36fb80d4
Mathias COUTANT <mathias.coutant@pxcom.aero>
2025-01-22 22:51:01 +0100

    Update payment stripe

--- back/src/routes/paymentRoutes.js
+++ back/src/routes/paymentRoutes.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { simulatePurchase } from "../controllers/paymentController.js";
+import {
+  simulatePurchase,
+  createPaymentIntent,
+} from "../controllers/paymentController.js";
 
 const router = express.Router();
 
 // Routes pour les paiements
+router.post("/stripe/clientSecret", createPaymentIntent);
 router.post("/stripe/add", simulatePurchase);
 
 export default router;
