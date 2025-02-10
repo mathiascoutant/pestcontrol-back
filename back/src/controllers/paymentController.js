d3ca7e77903bb23430e680c37b232fa594347542
Mathias COUTANT <mathias.coutant@pxcom.aero>
2025-02-10 10:18:21 +0100

    tests

--- back/src/controllers/paymentController.js
+++ back/src/controllers/paymentController.js
@@ -17,8 +17,6 @@ if (!process.env.STRIPE_SECRET_KEY) {
   );
 }
 
-console.log(process.env.STRIPE_SECRET_KEY);
-
 // Utiliser la clé secrète Stripe
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
